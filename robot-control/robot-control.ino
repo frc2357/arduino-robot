6e69b031efe676fdfdaa479e5978fd76b9a77f9d
coderkevin <kevin@redfenix.com>
2021-07-05 19:48:16 -0500

    Add base robot-control sketch

---
+++ robot-control/robot-control.ino
@@ -0,0 +1,16 @@
+/*
+ * robot-control.ino
+ * 
+ * This is the main control program for the robot controller.
+ * It can receive control information via i2c and then
+ * controls the individual motors, solenoids, and devices.
+ */
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  delay(1000);                       // wait for a second
+}
