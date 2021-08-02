f82e1667457216cda541fb45639cf3908f6dcdc4
NC2357 <crazyfloob@gmail.com>
2021-08-01 19:46:29 -0500

    Added min and max values for the modifiable integer.

--- human-controls/src/DisplayPage.h
+++ human-controls/src/DisplayPage.h
@@ -7,13 +7,14 @@
 #include "Printer.h"
 
 //For array of printers:
-//A printer with text value of "!modifyValue! indicates where the modifiable integer will be inserted"
+//A printer with text value of "!modifyValue!" indicates where the modifiable integer will be inserted and will be updated by Json state"
+//A printer with text prefixed by "!u!" indicates a field to be updated by Json state. The following text is the field name
 class DisplayPage : DisplayInterface
 {
 public:
     DisplayPage();
-    DisplayPage(int increment, bool isActivate, int printersLength, Printer printers[]);
-    void displayPageInit(int increment, bool isActivate, int printersLength, Printer printers[]);
+    DisplayPage(int increment, int min, int max, bool isActivate, int printersLength, Printer printers[]);
+    void displayPageInit(int increment, int min, int max, bool isActivate, int printersLength, Printer printers[]);
     ~DisplayPage();
     void cleanUp(DisplayController &display);
     void paint(DisplayController &display, bool isActive);
@@ -23,7 +24,7 @@ public:
 
 private:
     bool isActivate;
-    int modifyValue, increment, printersLength;
+    int modifyValue, min, max, increment, printersLength;
     Printer *printers;
 };
 #endif
