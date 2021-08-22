05040861b4bda8372f9a179c6aa5ae3d1b1886a1
NC2357 <crazyfloob@gmail.com>
2021-08-22 15:10:09 -0500

    Remove comment

--- HumanControls/Page.h
+++ HumanControls/Page.h
@@ -5,9 +5,6 @@
 #include "RotaryKnobController.h"
 #include "DisplayController.h"
 
-//For array of printers:
-//A printer with text value of "!modifyValue!" indicates where the modifiable integer will be inserted and will be updated by Json state"
-//A printer with text prefixed by "!u!" indicates a field to be updated by Json state. The following text is the field name
 class Page : DisplayInterface
 {
 public:
