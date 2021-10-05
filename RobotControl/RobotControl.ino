f3ac4de04214e3eb84d8b82ee984b3b3bd01d131
coderkevin <kevin@redfenix.com>
2021-10-05 16:19:02 -0500

    RobotControl: Adjust state to be smaller in bytes

--- RobotControl/RobotControl.ino
+++ RobotControl/RobotControl.ino
@@ -11,40 +11,32 @@
 #include "Robot.h"
 #include "StatusLEDs.h"
 
-#define SOFTWARE_VERSION   "1"
+#define SOFTWARE_VERSION   1
 #define SERIAL_BAUD_RATE   115200
 #define I2C_HOST_ADDRESS   0x04
 #define I2C_DEVICE_ADDRESS 0x08
 #define PIN_LED_BUILTIN    LED_BUILTIN
 
 // Set up the JSON State for the robot
-JsonElement humanControlFields[] = {
-  Json::Int("id", -1),
-  Json::String("conn", "Disconnected"),
-};
-
 JsonElement eStopFields[] = {
   Json::Boolean("btn", true),
 };
 
 JsonElement robotStateFields[] = {
-  Json::String("ver", SOFTWARE_VERSION),
-  Json::Int("tck", 0),
-  Json::Int("avgTck", 0),
-  Json::Int("up", 0),
+  Json::Int("v", SOFTWARE_VERSION),
+  Json::Int("t", 0),
   Json::String("status", "Disabled"),
-  Json::Object("hCtrl", humanControlFields),
   Json::Object("eStop", eStopFields),
-  Json::Float("bat", 0.0),
-  Json::Float("angle", 0.0),
-  Json::Float("tnkPres", 0.0),
-  Json::Float("frPres", 0.0),
-  Json::Int("vlvTm", 0),
-  Json::Float("dVel", 0.0),
-  Json::Float("dRot", 0.0),
+  Json::Float("b", 0.0),
+  Json::Int("ang", 0),
+  Json::Int("tPr", 0),
+  Json::Int("fPr", 0),
+  Json::Int("vlv", 0),
+  Json::Float("x", 0.0),
+  Json::Float("y", 0.0),
   Json::Boolean("fire", false),
   Json::Boolean("fill", false),
-  Json::String("err", "", 32),
+  Json::String("err", "", 16),
 };
 JsonElement robotState = Json::Object(robotStateFields);
 
