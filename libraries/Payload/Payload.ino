9ea714a52248431442d6381f558912b0d6874626
NolanCampbell2357 <nolanc1223@gmail.com>
2022-06-16 17:21:01 -0500

    Update struct (Still broken)

--- libraries/Payload/Payload.ino
+++ libraries/Payload/Payload.ino
@@ -4,17 +4,15 @@ TShirtCannonPayload cannon;
 void setup()
 {
     Serial.begin(115200);
-    cannon.m_payload->messageType = 2;
-    cannon.m_payload->messageIndex = 16;
-    cannon.m_payload->angle = 0B1111111;
-    // memset
+    cannon.m_payload.angle = 0B1111111;
+
     Serial.print("Set as: ");
-    Serial.println(cannon.m_payload->angle);
+    Serial.println(cannon.m_payload.angle);
     Serial.println("Converting to character array");
     uint8_t *trans = cannon.buildTransmission();
     cannon.readMessage(trans);
     Serial.print("After conversions: ");
-    Serial.println(cannon.m_payload->angle);
+    Serial.println(cannon.m_payload.angle);
 
     cannon.print();
 }
