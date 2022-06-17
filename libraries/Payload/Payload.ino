5c1b41ae6f56cf46676b18a5a0535a7bb82c6186
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2022-06-16 17:57:10 -0700

    Updated transmission building

--- libraries/Payload/Payload.ino
+++ libraries/Payload/Payload.ino
@@ -4,17 +4,22 @@ TShirtCannonPayload cannon;
 void setup()
 {
     Serial.begin(115200);
-    cannon.m_payload.angle = 0B1111111;
+    // cannon.m_payload->message = 0B1111111;
 
-    Serial.print("Set as: ");
-    Serial.println(cannon.m_payload.angle);
-    Serial.println("Converting to character array");
-    uint8_t *trans = cannon.buildTransmission();
-    cannon.readMessage(trans);
-    Serial.print("After conversions: ");
-    Serial.println(cannon.m_payload.angle);
+//    Serial.print("Set as: ");
+//    Serial.println(cannon.m_payload->message);
+//    Serial.println("Converting to character array");
+    uint8_t transmission = cannon.buildTransmission();
+    
+    // for (int i = 0; i < sizeof(transmission); i++) {
+    //     Serial.println(transmission[i]);
+    // }
 
-    cannon.print();
+//    cannon.readMessage(trans);
+//    Serial.print("After conversions: ");
+//    Serial.println(cannon.m_payload->message);
+
+    // cannon.print();
 }
 
 void loop()
--- libraries/Payload/TShirtCannonPayload.h
+++ libraries/Payload/TShirtCannonPayload.h
@@ -8,27 +8,25 @@ class TShirtCannonPayload
 
 private:
     static const int PAYLOAD_LENGTH; // Length of payload in bytes
-    struct Payload
-    {
-        uint32_t messageType : 2;          // 0=From robot, 1=From controller, 2=no op from controller, 3=drive x/y from controller
-        uint32_t messageIndex : 5;         // Count up from 0, then roll over from 31 back to 0
-        uint32_t status : 3;               // 0=disabled, 1=enabled, 2=adjusting (cannot prime or fire), 3=primed, 4=firing
-        uint32_t error : 2;                // 0=no error, 1=e-stop, 2=no controller, 3=other (check logs)
-        uint32_t controllerDriveLeft : 8;  // Specific to motor controller
-        uint32_t controllerDriveRight : 8; // Specific to motor controller
-        uint32_t batteryVoltage : 2;       // 0= <20, 1= 20-22, 2= 22-24,3= 24+
-        uint32_t angle : 7;                // 0=down, 127=up
-        uint32_t tankPressure : 7;         // In PSI: 0-127
-        uint32_t firingPressure : 7;       // In PSI: 0-127
-        uint32_t firingTime : 5;           // 0=100ms, 20=300ms
-    } __attribute__((__packed__));
+    static const int DATA_LENGTH;   
+    uint8_t m_messageType;          // 0=From robot, 1=From controller, 2=no op from controller, 3=drive x/y from controller
+    uint8_t m_messageIndex;         // Count up from 0, then roll over from 31 back to 0
+    uint8_t m_status;               // 0=disabled, 1=enabled, 2=adjusting (cannot prime or fire), 3=primed, 4=firing
+    uint8_t m_error;                // 0=no error, 1=e-stop, 2=no controller, 3=other (check logs)
+    uint8_t m_controllerDriveLeft;  // Specific to motor controller
+    uint8_t m_controllerDriveRight; // Specific to motor controller
+    uint8_t m_batteryVoltage;       // 0= <20, 1= 20-22, 2= 22-24,3= 24+
+    uint8_t m_angle;                // 0=down, 127=up
+    uint8_t m_tankPressure;         // In PSI: 0-127
+    uint8_t m_firingPressure;       // In PSI: 0-127
+    uint8_t m_firingTime;           // 0=100ms, 20=300ms
+
 
 public:
     TShirtCannonPayload();
-    uint8_t *buildTransmission();
+    uint8_t buildTransmission();
     boolean readMessage(uint8_t *message);
     void print();
 
-    Payload m_payload;
 };
 #endif
