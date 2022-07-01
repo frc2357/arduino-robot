23d96db9c78eee81fa8380a879b8b681b10ed8ec
NolanCampbell2357 <nolanc1223@gmail.com>
2022-06-30 19:53:44 -0500

    Refine transmission building

--- libraries/Payload/Payload.ino
+++ libraries/Payload/Payload.ino
@@ -4,25 +4,11 @@ TShirtCannonPayload cannon;
 void setup()
 {
     Serial.begin(115200);
-    // cannon.m_payload->message = 0B1111111;
-
-//    Serial.print("Set as: ");
-//    Serial.println(cannon.m_payload->message);
-//    Serial.println("Converting to character array");
-    uint8_t transmission = cannon.buildTransmission();
-    
-    // for (int i = 0; i < sizeof(transmission); i++) {
-    //     Serial.println(transmission[i]);
-    // }
-
-//    cannon.readMessage(trans);
-//    Serial.print("After conversions: ");
-//    Serial.println(cannon.m_payload->message);
-
-    // cannon.print();
 }
 
 void loop()
 {
     // cannon.print();
+    uint8_t *transmission = cannon.buildTransmission();
+    delay(2000);
 }
